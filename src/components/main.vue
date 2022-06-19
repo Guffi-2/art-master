17adbee06424f1bba1c537fd3745bef1eae5db0f
Guffi-2 <76037676+Guffi-2@users.noreply.github.com>
2022-06-19 14:29:53 +0000

    commit

---
+++ src/components/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="HelloWorld">
+    <h1>Hi!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #4a5059;
+}
+</style>
